f61a9450b5c324f26cb95e15bf50879e1c800c0f
Joël Mbiapa <94489918+Personne73@users.noreply.github.com>
2024-11-20 13:46:56 +0100

    docs: report project 1

---
+++ Project2/src/Main.java
@@ -0,0 +1,13 @@
+package Project2.src;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
